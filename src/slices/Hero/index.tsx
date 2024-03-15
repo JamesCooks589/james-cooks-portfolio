c4515c6a96fa4573770121ff39ad896779b4bb88
SoMuchSalt <Oplayer589@gmail.com>
2024-03-16 00:34:02 +0200

    Setting up hero page and GSAP

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -1,5 +1,7 @@
-import { Content } from "@prismicio/client";
+import {useEffect, useRef} from "react";
+import { Content, KeyTextField } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
+import gsap from "gsap";
 
 /**
  * Props for `Hero`.
@@ -10,12 +12,38 @@ export type HeroProps = SliceComponentProps<Content.HeroSlice>;
  * Component for "Hero" Slices.
  */
 const Hero = ({ slice }: HeroProps): JSX.Element => {
+  const component = useRef(null);
+
+  useEffect(() => {
+    let ctx = gsap.context(()=>{}, component);
+    return () => ctx.revert();
+  }, []);
+
+
+  const renderLetters = (name: KeyTextField, key: string) => {
+    if(!name) return;
+      return name.split('').map((letter, index) => (
+        <span key={index} className={`name-animation name-animation-${key} inline-block opacity-0`}>
+          {letter}
+        </span>
+      ))
+  }
   return (
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-      Placeholder component for hero (variation: {slice.variation}) Slices
+      <div className="grid-min-h-[70vh] md:grid-cols-2 items-center">
+        <div className="col-start-1 md:row-start-1">
+          <h1 className="mb-8 text-[clamp(3rem,20vmin,20rem)] font-extrabold leading-none tracking-tighter" aria-label={slice.primary.first_name + " " + slice.primary.last_name}>
+            <span className="block text-slate-300">{renderLetters(slice.primary.first_name, "first")}</span>
+            <span className="-mt-[.2em] block text-slate-500">{renderLetters(slice.primary.last_name, "last")}</span>
+          </h1>
+          <span className="block bg-gradient-to-tr from-yellow-500 via-yellow-200 to-yellow-500 bg-clip-text text-2xl
+           font-bold uppercase tracking-[.2em] text-transparent opacity-100 md:text-4xl">{slice.primary.tag_line}</span>
+        </div>
+        
+      </div>
     </section>
   );
 };
