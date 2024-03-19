dab7cf8793872e18b930b685a93f9938bc1f10c9
SoMuchSalt <Oplayer589@gmail.com>
2024-03-19 17:16:56 +0200

    Add new sound files and dependencies, create Header component, update Bounded component, and modify Hero component

--- src/components/Bounded.tsx
+++ src/components/Bounded.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import clsx from "clsx";
+
 type BoundedProps = {
     as? : React.ElementType;
     className? : string;
@@ -8,12 +10,13 @@ type BoundedProps = {
 const Bounded =  React.forwardRef<HTMLDivElement, BoundedProps>(
     ({as : Comp = "section", className, children, ...restProps}, ref) => {
         return (
-            <Comp ref ={ref} className={"px-4 py-10 md:px-6 md:py-14 lg:py-16"}
+            <Comp 
+            ref ={ref}
+            className={clsx("px-4 py-10 md:px-6 md:py-14 lg:py-16", className)}
             {...restProps}>
                 <div className="mx-auto w-full max-w-7xl">
                     {children}
                 </div>
-                {children}
             </Comp>
         )
     }
---
+++ src/components/Header.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { createClient } from "@/prismicio";
+
+export default async function Header(){
+    const client = createClient();
+    const settings = await client.getSingle("settings");
+    return (
+        <header className="top-0 z-50 mx-auto max-w-7xl md:sticky md:top-4">
+            <nav>
+                <ul>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </nav>
+
+        </header>
+    );
+}
