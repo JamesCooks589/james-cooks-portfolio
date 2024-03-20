f2a3500cb54f9cbbe9e0e3f64708e649632725ef
SoMuchSalt <Oplayer589@gmail.com>
2024-03-20 18:57:51 +0200

    Add new files and update Tailwind CSS configuration

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { createClient } from "@/prismicio";
+import  Link  from "next/link";
+import { PrismicNextLink } from "@prismicio/next";
 
 export default async function Header(){
     const client = createClient();
@@ -8,8 +10,20 @@ export default async function Header(){
         <header className="top-0 z-50 mx-auto max-w-7xl md:sticky md:top-4">
             <nav>
                 <ul>
-                    <li></li>
-                    <li></li>
+                    <li>
+                        <Link href="/" aria-label="Home Page">
+                            {settings.data.name}
+                        </Link>
+                            
+                    </li>
+                    {settings.data.nav_item.map(({link, label}, index) => (
+                        <li key={index}>
+                            <PrismicNextLink field={link} >
+                                {label}
+                            </PrismicNextLink>
+                        </li>
+                    ))}
+                    
                 </ul>
             </nav>
 
